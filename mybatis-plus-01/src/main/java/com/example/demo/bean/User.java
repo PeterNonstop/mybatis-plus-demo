2ca3cc65a1b30e6ae8b3264841e1afad24c4fdc3
Peter <Xiao_Peter@syscom.com.cn>
2021-11-08 01:09:30 +0800

    Initial commit

---
+++ mybatis-plus-01/src/main/java/com/example/demo/bean/User.java
@@ -0,0 +1,68 @@
+package com.example.demo.bean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * To change it use File | Settings | Editor | File and Code Templates.
+ *
+ * @author Peter
+ * @date 2021/11/8 0:34
+ * @description TODO
+ */
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public User(String id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public User() {
+    }
+}
---
+++ mybatis-plus-01/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.bean.User;
+
+/**
+ * Created with IntelliJ IDEA.
+ * To change it use File | Settings | Editor | File and Code Templates.
+ *
+ * @author Peter
+ * @date 2021/11/8 0:36
+ * @description TODO
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
